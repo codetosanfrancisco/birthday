1ed55417de909811cd0e299211bce315d4cd6888
codetosanfrancisco <codetosanfrancisco@gmail.com>
2019-08-29 02:46:08 +0800

    Email Automation Implemented

--- server/src/Scheduler/Scheduler.js
+++ server/src/Scheduler/Scheduler.js
@@ -5,28 +5,35 @@ const moment = require("moment");
 
 
 var rule = new schedule.RecurrenceRule();
-rule.second = 1;
+rule.dayOfWeek = [0, 6];
+rule.hour = 8;
+rule.minute = 0;
 
 schedule.scheduleJob(rule, function(){
     User.find({}, function(err, users) {
         if(!err){
-            //Check for user where today is birthday
-            //push user into an array
-            //loop the array and send email accordingly
-            const mailOptions = {
-                from: process.env.GOOGLE_EMAIL_USER,
-                to: "voonshunzhi@gmail.com",
-                subject: 'YES UNMC testing',
-                html: "<div>Hello World YES UNMC from Voon!</div>"
-              }
-            Mailer.smtpTransport.sendMail(mailOptions, function(error, info) {
-                if (error) {
-                  console.log("Failed to send birthday email");
-                } else {
-                  console.log(`Succesfully sent to ${mailOptions.to}`, info.messageId, info.response);
-                }
-                Mailer.smtpTransport.close();
+            var today = moment.utc();
+            users = users.filter(user => {
+                var user_birthday = moment(user.birthday)
+                return user_birthday.diff(today,'days') === 0;
             });
+            console.log(users);
+            for(let i = 0; i < users.length; i++) {
+                const mailOptions = {
+                    from: process.env.GOOGLE_EMAIL_USER,
+                    to: users[i].email,
+                    subject: 'YES UNMC testing',
+                    html: `<div>Hello World YES UNMC from Voon to ${users[i].name}!</div>`
+                  }
+                Mailer.smtpTransport.sendMail(mailOptions, function(error, info) {
+                    if (error) {
+                      console.log("Failed to send birthday email");
+                    } else {
+                      console.log(`Succesfully sent to ${mailOptions.to}`, info.messageId, info.response);
+                    }
+                    Mailer.smtpTransport.close();
+                });
+            }
         }
     });
 });

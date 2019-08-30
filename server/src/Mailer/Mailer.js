c9b4b1479572936a172f0ec5e6fc77fb6e0ca635
codetosanfrancisco <codetosanfrancisco@gmail.com>
2019-08-31 00:42:06 +0800

    Email Template Completed

--- server/src/Mailer/Mailer.js
+++ server/src/Mailer/Mailer.js
@@ -1,17 +1,18 @@
-var nodemailer = require('nodemailer');
-require('dotenv').config()
+var nodemailer = require("nodemailer");
+require("dotenv").config();
+const Email = require("email-templates");
+const email = new Email();
 
 var smtpTransport = nodemailer.createTransport("SMTP", {
-    service: "Gmail",
-    auth: {
-      XOAuth2: {
-        user: process.env.GOOGLE_EMAIL_USER, // Your gmail address.
-        clientId: process.env.GOOGLE_MAIL_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_MAIL_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
-      }
+  service: "Gmail",
+  auth: {
+    XOAuth2: {
+      user: process.env.GOOGLE_EMAIL_USER, // Your gmail address.
+      clientId: process.env.GOOGLE_MAIL_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_MAIL_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN
     }
-  });
+  }
+});
 
-
-module.exports = {smtpTransport};
+module.exports = { smtpTransport };
--- server/src/Scheduler/Scheduler.js
+++ server/src/Scheduler/Scheduler.js
@@ -2,37 +2,45 @@ var schedule = require("cron").CronJob;
 var Mailer = require("../Mailer/Mailer");
 var User = require("../user_schema");
 const moment = require("moment");
+const Email = require("email-templates");
+const email = new Email();
 
 new schedule(
   "0 8 * * 0-6",
+  //"* * * * *",
   function() {
     User.find({}, function(err, users) {
       if (!err) {
-        var today = moment.utc();
         users = users.filter(user => {
           var user_birthday = moment(user.birthday);
-          return user_birthday.diff(today, "days") === 0;
+          return user_birthday.isSame(new Date(), "day");
         });
-        console.log(users);
         for (let i = 0; i < users.length; i++) {
-          const mailOptions = {
-            from: process.env.GOOGLE_EMAIL_USER,
-            to: users[i].email,
-            subject: "YES UNMC testing",
-            html: `<div>Hello World YES UNMC from Voon to ${users[i].name}!</div>`
-          };
-          Mailer.smtpTransport.sendMail(mailOptions, function(error, info) {
-            if (error) {
-              console.log("Failed to send birthday email");
-            } else {
-              console.log(
-                `Succesfully sent to ${mailOptions.to}`,
-                info.messageId,
-                info.response
-              );
-            }
-            Mailer.smtpTransport.close();
-          });
+          email
+            .render("birthday/template1", {
+              name: users[i].name,
+              date: moment(users[i].date).format("MMM Do")
+            })
+            .then(html => {
+              const mailOptions = {
+                from: process.env.GOOGLE_EMAIL_USER,
+                to: users[i].email,
+                subject: "YES UNMC testing",
+                html: html
+              };
+              Mailer.smtpTransport.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                  console.log("Failed to send birthday email");
+                } else {
+                  console.log(
+                    `Succesfully sent to ${mailOptions.to}`,
+                    info.messageId,
+                    info.response
+                  );
+                }
+                Mailer.smtpTransport.close();
+              });
+            });
         }
       }
     });
